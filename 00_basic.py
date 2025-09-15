ff39ba9f2b0827c753fd675a03f7a15e57437859
SagarBhadra01 <sagarbhadra404@gmail.com>
2025-09-15 22:33:07 +0530

    Create 00_basic.py
    basic structure

---
+++ 00_basic.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+#This is a comment
+
+
+#input and output data
+name = input("Enter your name: ")
+print("Hello,", name, "! Welcome!")
+x = int(input("enter first number:"))
+y= int(input("enter 2nd number:"))
+print(x+y)
+
+
+# taking two inputs at a time
+x, y = input("Enter two values: ").split()
+print(x+y)
